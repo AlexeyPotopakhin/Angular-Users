622e30aebf78559520f6614d77f9b509f45df4bc
Alexey Potopakhin <alexey_potopahin@hotmail.com>
2019-05-19 23:24:32 +0300

    Working on app-dropdown

--- src/app/controls/controls.module.ts
+++ src/app/controls/controls.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {InputComponent} from './input/input.component';
 import {FormsModule} from '@angular/forms';
+import { DropdownComponent } from './dropdown/dropdown.component';
 
 @NgModule({
-  declarations: [InputComponent],
-  exports: [InputComponent],
+  declarations: [InputComponent, DropdownComponent],
+  exports: [InputComponent, DropdownComponent],
   imports: [
     CommonModule,
     FormsModule
---
+++ src/app/controls/dropdown/dropdown.component.ts
@@ -0,0 +1,81 @@
+import {Component, forwardRef, Input} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
+import {noop} from 'rxjs';
+import * as _ from 'lodash';
+
+@Component({
+  selector: 'app-dropdown',
+  template: `
+    <div class="app-input__container" (click)="isDropdownOpen = !isDropdownOpen">
+      <input [ngModel]="name" (blur)="onBlur()" [required]="required" [placeholder]="placeholder" readonly>
+      <i class="material-icons">arrow_drop_down</i>
+    </div>
+    <div class="app-dropdown__options" *ngIf="isDropdownOpen">
+      <div class="app-dropdown__scroll-host">
+        <div class="app-dropdown__option" (click)="selectOption(option)" *ngFor="let option of values">{{option['name']}}</div>
+      </div>
+    </div>
+  `,
+  styleUrls: ['../input/input.component.styl', './dropdown.component.styl'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => DropdownComponent),
+      multi: true
+    }
+  ]
+})
+export class DropdownComponent implements ControlValueAccessor {
+  @Input() placeholder: string;
+  @Input() required: boolean = false;
+  @Input() values: [{key: string; name: string}];
+
+  isDropdownOpen: boolean = false;
+
+  private innerKey: any;
+
+  constructor() { }
+
+  private onTouchedCallback: () => void = noop;
+  private onChangeCallback: (_: any) => void = noop;
+
+  get name(): any {
+    if (_.isNil(this.key))
+      return;
+    return _.find(this.values, ['key', this.key]).name;
+  }
+
+  get key(): any {
+    return this.innerKey;
+  };
+
+  set key(v: any) {
+    if (v !== this.innerKey) {
+      this.innerKey = v;
+      this.onChangeCallback(v);
+    }
+  }
+
+  onBlur() {
+    this.onTouchedCallback();
+  }
+
+  registerOnChange(fn: any): void {
+    this.onChangeCallback = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.onTouchedCallback = fn;
+  }
+
+  writeValue(obj: any): void {
+    if (obj !== this.innerKey) {
+      this.innerKey = obj;
+    }
+  }
+
+  selectOption(option: {key: string; name: string}) {
+    this.isDropdownOpen = false;
+    this.key = option.key;
+  }
+}
